045d0d7aa59e394ebf5592951229aab089c6e832
quentin <quentin@localhost.localdomain>
2015-10-04 12:20:45 +0200

    Modif du script de lancement

--- launch-test.sh
+++ launch-test.sh
@@ -18,37 +18,49 @@ cd $sources
 cmd_base='java Simulateur -s '
 
 echo '######### Début du test 1  #########'
-echo "$cmd_base"
+echo "$cmd_base -mess 20 -seed 10"
 echo ""
-$cmd_base 
+$cmd_base -mess 20 -seed 10
 echo '######### Fin du test 1 #########'
 
 echo '######### Début du test 2  #########'
-echo "$cmd_base -mess 11111111111111111"
+echo "$cmd_base -mess 20 -seed 10 -form NRZ"
 echo ""
-$cmd_base -mess 11111111111111111         
+$cmd_base -mess 20 -seed 10 -form NRZ         
 echo '######### Fin du test 2 #########'
 
 echo '######### Début du test 3  #########'
-echo "$cmd_base -mess 15"
+echo "$cmd_base -mess 20 -seed -form NRZT"
 echo ""
-$cmd_base -mess 15
+$cmd_base -mess 20 -seed 10 -form NRZT
 echo '######### Fin du test 3 #########'
 
 echo '######### Début du test 4  #########'
-echo "$cmd_base -seed 10"
+echo "$cmd_base -form NRZT -ampl -2 2"
 echo ""
-$cmd_base -seed 10
+$cmd_base -form NRZT -ampl -2 2
 echo '######### Fin du test 4 #########'
 
 echo '######### Début du test 5  #########'
-echo "$cmd_base -seed 10"
+echo "$cmd_base -form NRZT -ampl -2 1"
 echo ""
-$cmd_base -seed 10
+$cmd_base -form NRZT -ampl -2 1
 echo '######### Fin du test 5 #########'
 
 echo '######### Début du test 6  #########'
-echo "$cmd_base -seed 10 -mess 10"
+echo "$cmd_base -form NRZT -ampl -2 5"
 echo ""
-$cmd_base -seed 10 -mess 10
+$cmd_base -form NRZT -ampl -2 5
 echo '######### Fin du test 6 #########'
+
+echo '######### Début du test 7  #########'
+echo "$cmd_base -form NRZT -ampl 1 5"
+echo ""
+$cmd_base -form NRZT -ampl 1 5
+echo '######### Fin du test 7 #########'
+
+echo '######### Début du test 8  #########'
+echo "$cmd_base -form NRZT -mess 40 -nbEch 150"
+echo ""
+$cmd_base -form NRZT -mess 40 -nbEch 150
+echo '######### Fin du test 8 #########'
