60c5e059a5b2f62c763c67840295af10ecc77173
Quentin VALLIN <qvallin@pc-info-295.priv.enst-bretagne.fr>
2015-10-05 18:58:02 +0200

    Correction très moche du RZ mais ça marche :D. On se croit chez Microsoft !

--- src/transmetteurs/EmetteurAnalogique.java
+++ src/transmetteurs/EmetteurAnalogique.java
@@ -407,6 +407,11 @@ public class EmetteurAnalogique extends Transmetteur<Boolean,Float> {
 						}
 						if(i>= nbEch* 1/3 && i < nbEch * 5/12) { // On monte 1/3 à 5/12
 							prec += (12*max/nbEch);
+							
+							if(prec > max) {
+								prec = max;
+							}
+							
 							informationEmise.add(prec);
 						}
 						if(i >= nbEch * 5/12 && i < nbEch* 7/12 ) { // On reste au max de 5/12 à 7/12
@@ -414,7 +419,11 @@ public class EmetteurAnalogique extends Transmetteur<Boolean,Float> {
 							prec = max;
 						}
 						if(i>= nbEch * 7/12 && i< nbEch * 2/3) { // On descend de 7/12 à 2/3
-							prec-= (12*max/nbEch);
+							prec-= (12*max/(nbEch));
+							
+							if(prec < 0) {
+								prec = 0.0f;
+							}
 							informationEmise.add(prec);
 						}
 						if(i >= (nbEch * 2/3)) { //Reste à 0 de 2/3 à 1
@@ -429,6 +438,11 @@ public class EmetteurAnalogique extends Transmetteur<Boolean,Float> {
 						
 						if(i>= nbEch* 1/3 && i < nbEch * 5/12) { // On monte de 1/3 à 5/12
 							prec -= Math.abs(12*min/nbEch);
+							
+							if(prec < min) {
+								prec = min;
+							}
+							
 							informationEmise.add(prec);
 						}
 						if(i >= nbEch * 5/12 && i < nbEch* 7/12 ) { // On bloque de 5/12 à 7/12
@@ -436,7 +450,12 @@ public class EmetteurAnalogique extends Transmetteur<Boolean,Float> {
 							prec = min;
 						}
 						if(i>= nbEch * 7/12 && i< nbEch * 2/3) { // On redescend de 7/12 à 2/3
-							prec += Math.abs(12*min/nbEch);
+							prec += Math.abs(12*min/(nbEch));
+							
+							if(prec > 0) {
+								prec = 0.0f;
+							}
+							
 							informationEmise.add(prec);
 						}
 						if(i >= (nbEch * 2/3)) { // On reste à 0 sur 2/3 à 1
