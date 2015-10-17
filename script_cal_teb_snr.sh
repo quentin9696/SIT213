b3facd502e1c86f8486c6d01840e352cc3e88b5e
quentin <quentin@localhost.localdomain>
2015-10-17 12:58:38 +0200

    modif du script

--- script_cal_teb_snr.sh
+++ script_cal_teb_snr.sh
@@ -6,8 +6,8 @@ FORME="NRZ"
 cmd_base="java -Xmx30G Simulateur -mess $NBMESS -ampl -1 1 -form $FORME -seed 15 -ti 1 60 0.5 "
 
 echo "$cmd_base" >> "$FORME.txt"
-
-for i in {-40..20}
+echo "snr,teb" >> "$FORME.txt"
+for i in {-60..10}
 do
 	output=`$cmd_base -snr $i |grep TEB |cut -f2 -d :`
 	echo "$i,$output" >> "$FORME.txt"
